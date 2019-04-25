2f2afb1d7573d07f0df7e80a7a6b681d25cd93de
Igor Babichev <himic@himic.ru>
2019-04-25 14:14:02 +0500

    fix

--- docker/mysql/create_base.sh
+++ docker/mysql/create_base.sh
@@ -4,19 +4,25 @@
 #MYPASSWD=$password
 #mysql -uroot -p$password -Bse "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYPASSWD';"
 
-MYPASSWD=$RANDOM$RANDOM$RANDOM
+#MYPASSWD=$RANDOM$RANDOM$RANDOM
 
-mysqladmin -u root password $MYPASSWD;
+#mysqladmin -u root password $MYPASSWD;
 
-mysql -uroot -p$MYPASSWD -e "CREATE DATABASE IF NOT EXISTS clo_statistic DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-mysql -uroot -p$MYPASSWD -e "CREATE USER 'clo_statistic'@'%' IDENTIFIED BY 'clo_statistic';"
-mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'clo_statistic'@'%';"
-mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'clo_statistic'@'localhost';"
-mysql -uroot -p$MYPASSWD -e "FLUSH PRIVILEGES;"
-sleep 5
+mysql -uroot -p$$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS clo_statistic DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -p$$MYSQL_ROOT_PASSWORD -e "CREATE USER 'clo_statistic'@'%' IDENTIFIED WITH mysql_native_password BY 'clo_statistic';"
+mysql -uroot -p$$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON * . * TO 'clo_statistic'@'%';"
+mysql -uroot -p$$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON * . * TO 'clo_statistic'@'localhost';"
+mysql -uroot -p$$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+#mysql -uroot -p$MYPASSWD -e "CREATE DATABASE IF NOT EXISTS clo_statistic DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+#mysql -uroot -p$MYPASSWD -e "CREATE USER 'clo_statistic'@'%' IDENTIFIED WITH mysql_native_password BY 'clo_statistic';"
+#mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'clo_statistic'@'%';"
+#mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'clo_statistic'@'localhost';"
+#mysql -uroot -p$MYPASSWD -e "FLUSH PRIVILEGES;"
+#sleep 5
 #mysql -uroot -p$MYPASSWD domain_statistic < /root/structure.sql
-touch /root/.my.cnf;
-echo "[client]" > /root/.my.cnf 
-echo "password=$MYPASSWD" >> /root/.my.cnf
+#touch /root/.my.cnf;
+#echo "[client]" > /root/.my.cnf
+#echo "password=$MYPASSWD" >> /root/.my.cnf
 
 echo "Done"
